bf5f88a52bd07dfd61ee33fe5140bb66e48561c0
Adriaan Moors <adriaan@lightbend.com>
2018-02-21 16:45:20 +0100

    minimalism

--- src/main/java/com/tikal/hudson/plugins/notification/workflow/NotificationSendStep.java
+++ src/main/java/com/tikal/hudson/plugins/notification/workflow/NotificationSendStep.java
@@ -9,57 +9,29 @@ import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractSynchronousNonBlockingStepExecution;
 import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.DataBoundSetter;
 
-import javax.annotation.Nonnull;
 import javax.inject.Inject;
-import java.util.logging.Logger;
 
 /**
- * Workflow step to send a webhook notification.
+ * Workflow step to send a webhook notification. For use in pipeline jobs where the job end notification sometimes doesn't get sent properly (no idea why).
  */
 public class NotificationSendStep extends AbstractStepImpl {
-    private static final Logger logger = Logger.getLogger(NotificationSendStep.class.getName());
-
-    private final @Nonnull String message;
-    @Nonnull
-    public String getMessage() {
-        return message;
-    }
-
     private final boolean done;
-    public boolean isDone() {
-        return done;
-    }
-
+    public boolean isDone() { return done; }
 
     @DataBoundConstructor
-    public NotificationSendStep(@Nonnull String message, boolean done) {
-        this.message = message;
+    public NotificationSendStep(boolean done) {
         this.done = done;
     }
 
-
     @Extension
     public static class DescriptorImpl extends AbstractStepDescriptorImpl {
-
-        public DescriptorImpl() {
-            super(NotificationSendStepExecution.class);
-        }
-
-        @Override
-        public String getFunctionName() {
-            return "notificationSend";
-        }
-
-        @Override
-        public String getDisplayName() {
-            return "Send Job Notification";
-        }
+        public DescriptorImpl() { super(NotificationSendStepExecution.class); }
+        @Override public String getFunctionName() { return "notificationSend"; }
+        @Override public String getDisplayName() { return "Send Job Notification"; }
     }
 
     public static class NotificationSendStepExecution extends AbstractSynchronousNonBlockingStepExecution<Void> {
-
         private static final long serialVersionUID = 1L;
 
         @Inject
@@ -67,20 +39,22 @@ public class NotificationSendStep extends AbstractStepImpl {
 
         @StepContextParameter
         private transient Run<?, ?> run;
+
         @StepContextParameter
         private transient TaskListener listener;
 
         @Override
         protected Void run() throws Exception {
-            logger.info("Notification for "+ run.getFullDisplayName() +": "+ run.getBuildStatusSummary().message +" building= "+ run.isBuilding());
-            logger.info(step.getMessage() + (step.done ? "completed" : "started" ));
-            Phase phase = step.done ? Phase.COMPLETED : Phase.STARTED;
+            listener.getLogger().println("Notification: "+ (step.done ? "done" : "working" ));
+            listener.getLogger().println("(Info from run: building= "+ run.isBuilding() +" "+
+                    run.getBuildStatusSummary().message +
+                    " duration= "+ run.getDuration() +")");
 
+            Phase phase = step.done ? Phase.COMPLETED : Phase.STARTED;
             phase.handle(run, listener, run.getTimeInMillis() + run.getDuration());
 
             return null;
         }
-
     }
 
 }
